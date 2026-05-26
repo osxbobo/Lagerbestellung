8b356e0450dc00ba02d19781d353cead27306277
osxbobo <149384848+osxbobo@users.noreply.github.com>
2026-05-26 11:20:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // Diese Nummer bei jedem GitHub Upload um 1 erhöhen
 // z.B. v2, v3, v4 ...
 // → Browser erkennt automatisch die neue Version und lädt alles neu
-const CACHE_VERSION = 'v130';
+const CACHE_VERSION = 'v131';
 const CACHE_NAME    = `lagerapp-${CACHE_VERSION}`;
 const BASE_PATH     = '/lagerapp_alpha';
 
