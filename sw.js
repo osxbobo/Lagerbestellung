c63313ab07ca438cd0f501b434d494b82a53aafe
osxbobo <149384848+osxbobo@users.noreply.github.com>
2026-05-29 23:33:00 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // Diese Nummer bei jedem GitHub Upload um 1 erhöhen
 // z.B. v2, v3, v4 ...
 // → Browser erkennt automatisch die neue Version und lädt alles neu
-const CACHE_VERSION = 'v166';
+const CACHE_VERSION = 'v167';
 const CACHE_NAME    = `lagerapp-${CACHE_VERSION}`;
 
 // Dateien die offline verfügbar sein sollen (relativ zum SW-Scope)
